a405c66b260d30cbdcb5b168ed77c46ec1aacbbf
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2022-04-10 17:20:34 +0100

    Add MakeFile

---
+++ SolarCoffee.Data/Models/CustomerAddress.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace SolarCoffee.Data.Models
+{
+    public class CustomerAddress
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        
+        [MaxLength(100)]
+        public string AddressLine1 { get; set; }
+        
+        [MaxLength(100)]
+        public string AddressLine2 { get; set; }
+        
+        [MaxLength(100)]
+        public string City { get; set; }
+        
+        [MaxLength(10)]
+        public string PostCode { get; set; }
+        
+        [MaxLength(64)]
+        public string Country { get; set; }
+    }
+}
